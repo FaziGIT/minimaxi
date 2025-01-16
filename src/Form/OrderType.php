571f3a89393c7dcd7c99bd4503e9ca8ce8f5f9e6
Loeryx <altea95430@gmail.com>
2025-01-16 12:10:14 +0100

    [FEAT] : Add crud and controller for entity

---
+++ src/Form/OrderType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use App\Entity\DiscountCode;
+use App\Entity\Order;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('totalPrice')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('status')
+            ->add('client', EntityType::class, [
+                'class' => Client::class,
+                'choice_label' => 'id',
+            ])
+            ->add('appliedDiscount', EntityType::class, [
+                'class' => DiscountCode::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Order::class,
+        ]);
+    }
+}
