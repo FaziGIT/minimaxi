bdeb67605305dba5a3436fa60c4b8b1d1198ee69
Loeryx <altea95430@gmail.com>
2025-01-26 19:24:28 +0100

    [FEAT] : Add home page style and redirection from home page to other pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,24 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findMostRecentProducts(4);
+        $topProducts = $productRepository->findTopRatedProducts(4);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'products' => $products,
+            'topProducts' => $topProducts,
         ]);
     }
+
+    #[Route('/products/new-arrivals', name: 'app_show_new_arrivals')]
+    public function showAll(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findMostRecentProducts(15);
+        return $this->render('product/show_new_arrivals.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
 }
